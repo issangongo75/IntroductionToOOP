c5742a0a1616fc4c0f872649bab6bdd694c8583b
Ngong_xj55@top-academy.ru <Ngong_xj55@top-academy.ru>
2024-06-29 19:56:28 +0300

    Methods out of class

--- String/main.cpp
+++ String/main.cpp
@@ -2,91 +2,109 @@
 using namespace std;
 #define delimiter "\n-------------------------------\n"
 
+
+
 class String
 {
 	int size;
 	char* str;
 public:
-	int get_size()const
-	{
-		return size;
-	}
-	const char* get_str()const
-	{
-		return str;
-	}
-	char* get_str()
-	{
-		return str;
-	}
-	explicit String(int size = 80):size(size), str(new char[size ] {})
-	{
-		//this->size = size;
-		//this->str = new char[size] {};
-		cout << "Constructor:\t" << this << endl;
-	}
-	String(const char str[]):String(strlen(str)+1)
-	{
-		//this->size = strlen(str) + 1;
-		//this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = str[i];
-		cout << "Constructor:\t" << this << endl;
-	}
-	String(const String& other):String(other.str)
-	{
-		//this->size = other.size;
-		//this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-		cout << "CopyConstructor:" << this << endl;
-	}
-	String(String&& other)noexcept:size(other.size),str(other.str)//r-value reference
-	{
-		//Shallow copy:
-		this->size = other.size;
-		this->str = other.str;	//Shallow copy
+	int get_size()const;
+	const char* get_str()const;
+	char* get_str();
 
-		//Reset other:
-		other.size = 0;
-		other.str = nullptr;
-		cout << "MoveConstructor:" << this << endl;
-	}
-	~String()
-	{
-		delete[]str;
-		cout << "Destructor:\t" << this << endl;
-	}
+
+	explicit String(int size = 80);
+	String(const char str[]);
+	String(const String& other);
+	String(String&& other)noexcept;
+	~String();
 	//       Operators
-	String& operator=(const String& other)
-	{
-		if (this == &other)return *this;
-		delete[] this->str;
-		this->size = other.size;
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-		cout << "CopyAssignemt:\t" << this << endl;
-		return *this;
-	}
-	String& operator=(String&& other)noexcept	//r-value reference
-	{
-		if (this == &other)return *this;
-		delete[] this->str;
-		this->size = other.size;
-		this->str = other.str;
-		other.size = 0;
-		other.str = nullptr;
-		cout << "MoveAssignment:\t" << this << endl;
-		return *this;
-	}
+	String& operator=(const String& other);
+	String& operator=(String&& other)noexcept;
 	//        Methos
-	void print()const
-	{
-		cout << "obj:\t\t" << this << endl;
-		cout << "Size:\t\t" << size << endl;
-		cout << "Addr:\t\t" << &str << endl;
-		cout << "Str:\t\t" << str << endl;
-		cout << delimiter << endl;
-	}
+	void print()const;
 };
+
+int String::get_size()const
+{
+	return size;
+}
+const char* String::get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+String::String(int size) :size(size), str(new char[size] {})
+{
+	//this->size = size;
+	//this->str = new char[size] {};
+	cout << "Constructor:\t" << this << endl;
+}
+String::String(const char str[]) :String(strlen(str) + 1)
+{
+	//this->size = strlen(str) + 1;
+	//this->str = new char[size] {};
+	for (int i = 0; i < size; i++)this->str[i] = str[i];
+	cout << "Constructor:\t" << this << endl;
+}
+String::String(const String& other) :String(other.str)
+{
+	//this->size = other.size;
+	//this->str = new char[size] {};
+	for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+	cout << "CopyConstructor:" << this << endl;
+}
+String::String(String&& other)noexcept :size(other.size), str(other.str)//r-value reference
+{
+	//Shallow copy:
+	this->size = other.size;
+	this->str = other.str;	//Shallow copy
+
+	//Reset other:
+	other.size = 0;
+	other.str = nullptr;
+	cout << "MoveConstructor:" << this << endl;
+}
+String::~String()
+{
+	delete[]str;
+	cout << "Destructor:\t" << this << endl;
+}
+//       Operators
+String& String::operator=(const String& other)
+{
+	if (this == &other)return *this;
+	delete[] this->str;
+	this->size = other.size;
+	this->str = new char[size] {};
+	for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+	cout << "CopyAssignemt:\t" << this << endl;
+	return *this;
+}
+String& String::operator=(String&& other)noexcept	//r-value reference
+{
+	if (this == &other)return *this;
+	delete[] this->str;
+	this->size = other.size;
+	this->str = other.str;
+	other.size = 0;
+	other.str = nullptr;
+	cout << "MoveAssignment:\t" << this << endl;
+	return *this;
+}
+//        Methos
+void String::print()const
+{
+	cout << "obj:\t\t" << this << endl;
+	cout << "Size:\t\t" << size << endl;
+	cout << "Addr:\t\t" << &str << endl;
+	cout << "Str:\t\t" << str << endl;
+	cout << delimiter << endl;
+}
 
 String operator+(const String& left, const String& right)
 {
